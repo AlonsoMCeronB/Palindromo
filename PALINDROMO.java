5e72c5aacbfc8205d9c18d076fecc8824d7f6672
AlonsoMCeronB <131682805+AlonsoMCeronB@users.noreply.github.com>
2023-05-04 00:49:58 -0600

    Add files via upload

---
+++ PALINDROMO.java
@@ -0,0 +1,28 @@
+/* @author:IAN ALEJANDRO CORREA LÓPEZ
+ * @mail: icorrealopez102@gmail.com 
+ * PRACTICA: PALINDROMO 
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String Pal = "ORO";
+        int inc = 0;
+        int des = Pal.length()-1;
+        boolean bError = false;
+        while ((inc<des) && (!bError)){
+
+            if (Pal.charAt(inc)==Pal.charAt(des)){
+                inc++;
+                des--;
+            } else {
+                bError = true;
+            }
+        }
+        if (!bError)
+            System.out.println(Pal + " es un PALINDROMO");
+        else
+            System.out.println(Pal + " NO es un palindromo");
+    }
+}
